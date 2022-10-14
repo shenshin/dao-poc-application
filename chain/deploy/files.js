18fa08b82c83bb6158670db53ff03110c29dcec5
Aleksandr Shenshin <shenshin@me.com>
2022-10-14 23:39:24 +0200

    copy contracts abi and addresses to frontend

--- chain/deploy/files.js
+++ chain/deploy/files.js
@@ -2,8 +2,17 @@ const path = require('path');
 
 const contracts = path.join(__dirname, 'deployments', 'contracts.json');
 const external = path.join(__dirname, 'deployments', 'external.json');
+const frontAbiDir = path.join(
+  __dirname,
+  '..',
+  '..',
+  'front',
+  'src',
+  'contracts',
+);
 
 module.exports = {
   contracts,
   external,
+  frontAbiDir,
 };
